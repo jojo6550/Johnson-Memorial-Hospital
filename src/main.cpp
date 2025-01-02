b9da778a6e7a1d2f097a17a032ec8cb7f4bb71ea
jojo6550 <josiah.johnson6550@gmail.com>
2025-01-01 22:12:03 -0500

    `Added Staff class with validation and input functionality`

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,94 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <limits>
+#include <regex>
+#include <cstdlib>
 #include "../Header Files/Login.h"
 #include "../Header Files/Patient.h"
-int main() {
-    Patient p("Jamin", "Johnson", "07/05/2006", 18, 'M', "(876)-322-4356", "Janice Johnson", "(876)-771-1865", "01/01/2025","01/01/2026", "Surgical", "P001");
-    p.saveToFile();
-    p.displayPatientData();
+
+class Staff{
+    private:
+        std::string name ,phoneNumber, staffId, qualifications;
+        char role;
+    public:
+        Staff(){
+            
+        }
+        Staff(std::string n, std::string pn, std::string sId, std::string q, char r){
+            name = n;
+            phoneNumber = pn;
+            staffId = sId;
+            qualifications = q;
+            role = r;
+        }
+
+        bool validatePhoneNumberFormat(const std::string& phoneNumber) {
+            std::regex phonePattern(R"(^\(\d{3}\)-\d{3}-\d{4}$)");
+            return regex_match(phoneNumber, phonePattern);
+        }
+        void setStaffInfo(){
+            std::cout<<"******************************************************"<<std::endl;
+            std::cout<<"Enter full name: ";
+            getline(std::cin, name);
+            while(true){
+                std::cout<<"Role (d = Doctor, n = Nurse, a = auxillary): ";
+                if(std::cin>>role && (role == 'd' || role == 'n' || role == 'a'||role == 'D' || role == 'N' || role == 'A')){
+                    break;
+                } else {
+                    std::cout<<"Invalid role.\n";
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                }
+            }
+            while(true){
+                std::cout<<"Enter phone number (xxx)-xxx-xxxx: ";
+                if(std::cin>>phoneNumber && validatePhoneNumberFormat(phoneNumber)){
+                    break;
+                } else {
+                    std::cout<<"Invalid phone number.\n";
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                }
+            }
+            while(true){
+                std::cout<<"Enter staff ID (must begin with D, N or A. e.g A001): ";
+                if(std::cin>>staffId && staffId.length() == 4){
+                    if(role == 'D' || role == 'd' ){
+                        if(staffId[0] == 'D'){
+                            break;
+                        } else {
+                            std::cout << "This is a doctor, prefix must be D\n";
+                            std::cin.clear();
+                            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                        }
+                    }
+                    if(role == 'N' || role == 'n'){
+                        if(staffId[0] == 'N'){
+                            break;
+                        } else {
+                            std::cout << "This is a nurse, prefix must be N\n";
+                        }
+                    }
+                    if(role == 'A' || role == 'a'){
+                        if(staffId[0] == 'A'){
+                            break;
+                        } else {
+                            std::cout << "This is an auxillary staff member, prefix must be N\n";
+                        }
+                    }
+                    
+                } else {
+                    std::cout<<"Invalid staff ID length.\n";
+                }
+            }
+        }
+
+};
+
+int main(){
+    Staff doctor;
+    doctor.setStaffInfo();
     return 0;
 }
 
